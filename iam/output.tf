575793b18dc58fdd64402b4328745956b021fa8a
aditya1608 <adi1234desh16@gmail.com>
2024-11-29 14:27:01 +0530

    sensitive = true added

--- iam/output.tf
+++ iam/output.tf
@@ -1,11 +1,14 @@
 output "login_password" {
   value = aws_iam_user_login_profile.aditya.password
+  sensitive = true
 }
 
 output "access_key_id" {
     value = aws_iam_access_key.aditya.id
+    sensitive = true
 }
 
 output "secret_key" {
     value = aws_iam_access_key.aditya.secret
+    sensitive = true
 }
