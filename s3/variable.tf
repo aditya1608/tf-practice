67281f2d911884b92e0a1813d0a4889275258377
aditya1608 <adi1234desh16@gmail.com>
2024-11-29 19:11:09 +0530

    edited variables

--- s3/variable.tf
+++ s3/variable.tf
@@ -3,7 +3,7 @@ variable "bucket_name1" {
   description = "Enter bucket name1"
 }
 
-variable "bucket_name1" {
+variable "bucket_name2" {
   type        = string
   description = "Enter bucket name2"
 }
