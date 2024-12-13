abc51f7735b14a408f8ae6d23a43b90dd2d0abf4
aditya1608 <adi1234desh16@gmail.com>
2024-12-13 22:50:59 +0530

    small change

--- 3tier/backend.tf
+++ 3tier/backend.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    security_groups = [ aws_security_group.bastion_sg.id ]
+    security_groups = [ aws_security_group.web_sg.id ]
   }
 
   ingress {
