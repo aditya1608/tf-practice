4a78cce6226ae7e9ee3a640b1e56c03a11bdce7b
aditya1608 <adi1234desh16@gmail.com>
2024-12-17 18:00:02 +0530

    import example

--- example/taint.tf
+++ example/taint.tf
@@ -2,12 +2,11 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-# resource "aws_instance" "adi" {
-#     ami = "ami-053b12d3152c0cc71"
-#     instance_type = "t2.micro"
-#     key_name = "aws_key"
-#     tags = {
-#         Name = "adi"
-#     }
-  
-# }
+resource "aws_instance" "adi" {
+    # ami = "ami-053b12d3152c0cc71"
+    # instance_type = "t2.micro"
+    # key_name = "aws_key"
+    # tags = {
+    #     Name = "adi"
+    # }
+}
