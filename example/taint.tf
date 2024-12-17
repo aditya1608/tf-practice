073b698089bb83b23d86cb67ce8146af2b3e1a1f
aditya1608 <adi1234desh16@gmail.com>
2024-12-17 18:03:39 +0530

    ex

--- example/taint.tf
+++ example/taint.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "adi" {
-    # ami = "ami-053b12d3152c0cc71"
-    # instance_type = "t2.micro"
-    # key_name = "aws_key"
-    # tags = {
-    #     Name = "adi"
-    # }
+    ami = "ami-053b12d3152c0cc71"
+    instance_type = "t2.micro"
+    key_name = "aws_key"
+    tags = {
+        Name = "adi"
+    }
 }
