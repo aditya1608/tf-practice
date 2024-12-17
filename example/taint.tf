55b04288b9ec24cb919f5964c6ef50c6b95b707b
aditya1608 <adi1234desh16@gmail.com>
2024-12-17 17:51:47 +0530

    test import

--- example/taint.tf
+++ example/taint.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "adi" {
-    ami = "ami-053b12d3152c0cc71"
-    instance_type = "t2.micro"
-    key_name = "aws_key"
-    tags = {
-        Name = "adi"
-    }
+# resource "aws_instance" "adi" {
+#     ami = "ami-053b12d3152c0cc71"
+#     instance_type = "t2.micro"
+#     key_name = "aws_key"
+#     tags = {
+#         Name = "adi"
+#     }
   
-}
+# }
