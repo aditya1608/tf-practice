df46e4ded451d627dab40dfb16eac478f85dc687
aditya1608 <adi1234desh16@gmail.com>
2024-12-18 11:55:10 +0530

    user

--- provisioners/pro.tf
+++ provisioners/pro.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "adi" {
 
         connection {
             type = "ssh"
-            user = "ubuntu"
+            user = "ec2-user"
             private_key = file("./aws_key.pem")
             host = self.public_ip
         }    
@@ -29,7 +29,7 @@ resource "aws_instance" "adi" {
 
         connection {
             type = "ssh"
-            user = "ubuntu"
+            user = "ec2-user"
             private_key = file("./aws_key.pem")
             host = self.public_ip
         }      
