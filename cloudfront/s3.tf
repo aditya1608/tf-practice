7b82b976a5b8560e25ee11786298daaec2f0b589
aditya1608 <adi1234desh16@gmail.com>
2024-12-22 16:55:39 +0530

    obj

--- cloudfront/s3.tf
+++ cloudfront/s3.tf
@@ -43,7 +43,7 @@ resource "aws_s3_object" "mbo" {
     bucket = aws_s3_bucket.mb.id
     key = "index.html"
     source = "./index.html"
-    acl = "public-read"
+    # acl = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "mbw" {
