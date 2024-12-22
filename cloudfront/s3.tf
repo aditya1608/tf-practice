df23f1f1072479e98cd1bee5ba2858780a6f62f9
aditya1608 <adi1234desh16@gmail.com>
2024-12-22 14:19:30 +0530

    static website using s3

---
+++ cloudfront/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "mb" {
+    bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "mbow" {
+  bucket = aws_s3_bucket.mb.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mba" {
+    bucket = aws_s3_bucket.mb.id
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false  
+}
+
+resource "aws_s3_bucket_versioning" "mbv" {
+    bucket = aws_s3_bucket.mb.id
+    versioning_configuration {
+        status = "Disabled"
+    }
+}
+
+resource "aws_s3_object" "mbo" { 
+    bucket = aws_s3_bucket.mb.id
+    key = "index.html"
+    source = "./index.html"
+}
+
+resource "aws_s3_bucket_website_configuration" "mbw" {
+    bucket = aws_s3_bucket.mb.id
+    index_document {
+      suffix = "index.html"
+    }  
+}
