fe83491edfc66c6fa7dd40fd74723718ba798748
aditya1608 <adi1234desh16@gmail.com>
2024-12-23 14:29:26 +0530

    comment

--- cloudfront/s3.tf
+++ cloudfront/s3.tf
@@ -43,7 +43,7 @@ resource "aws_s3_object" "mbo" {
     bucket = aws_s3_bucket.mb.id
     key = "index.html"
     source = "./index.html"
-    # content_type = "text/html"
+    content_type = "text/html"
     # acl = "public-read"
 }
 
