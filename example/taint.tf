c35105ec6665f06b5d9dd9f1bf57573b01823cb4
aditya1608 <adi1234desh16@gmail.com>
2025-01-01 23:36:54 +0530

    Update taint.tf

--- example/taint.tf
+++ example/taint.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" # Use a compatible AWS provider version
-    }
-  }
-
-  required_version = ">= 1.3.0" # Minimum Terraform version
-}
-
 provider "aws" {
     region = "ap-south-1"
 }
